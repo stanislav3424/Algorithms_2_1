9e545206176e979dfd8c1018c4de167f2b63914b
test <doudoladov_stanislav@mail.ru>
2025-04-25 11:12:53 +0300

    Add project files.

---
+++ Algorithms_2_1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void fibonacci(int lastNumber = 0)
+{
+    static int counter = 0;
+    static long long first = 0;
+    static long long second = 1;
+
+    if (lastNumber != 0)
+    {
+        counter = lastNumber;
+    }
+
+    if (counter <= 0)
+        return;
+
+    std::cout << first << std::endl;
+    long long next = first + second;
+    first = second;
+    second = next;
+
+    counter--;
+    fibonacci();
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int lastNumber = 0;
+    std::cout << "¬ведите n: ";
+    std::cin >> lastNumber;
+    fibonacci(lastNumber);
+
+    return 0;
+}
